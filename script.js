4f410a13a6528f1c6921ed27e536724c9037e6d8
grouchomarxc137-star <grouchomarxc137@gmail.com>
2025-10-27 22:48:59 +0100

    Added the logic for getting the user input using prompt and for determening his choice by a switch statement in a getHumanChoice function. Assuming human input will always be valid for this exercise

--- script.js
+++ script.js
@@ -16,3 +16,23 @@ let getComputerChoice = () => {
     }
     return computerChoice;
 }
+// human choice, p = papers, s = scissors, r = rock; assuming the human will always input valid input
+let getHumanChoice = (input) => {
+    let humanChoice;
+    switch(input){
+        case "p":
+            humanChoice = "paper";
+            break;
+        case "r":
+            humanChoice = "rock";
+            break;
+        case "s":
+            humanChoice = "scissors";
+            break;
+        default:
+            humanChoice = "N/A";
+    }
+    return humanChoice;
+}
+let p = prompt("Type 's' for scissors, 'r' for rock, 'p' for paper: ");
+console.log(getHumanChoice(p));
