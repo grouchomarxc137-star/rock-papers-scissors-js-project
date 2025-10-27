64c5b6f4197347df91ca1b3cf99638d10807943f
grouchomarxc137-star <grouchomarxc137@gmail.com>
2025-10-27 23:35:30 +0100

    Finished the game by adding playGame function and configuring the win game conditions but no input control or case sensitivity

--- script.js
+++ script.js
@@ -34,5 +34,75 @@ let getHumanChoice = (input) => {
     }
     return humanChoice;
 }
-let p = prompt("Type 's' for scissors, 'r' for rock, 'p' for paper: ");
-console.log(getHumanChoice(p));
+// Playing a round, determining the winner and returning it
+let playRound = (cc, hc) => {
+    let roundWinner;
+    if (hc == "rock" && cc == "scissors" || hc == "paper" && cc == "rock" || hc == "scissors" && cc == "paper"){
+        roundWinner = "human";
+    }
+    else if (hc === cc)
+    {
+        roundWinner = "draw";
+    }
+    else{
+        roundWinner = "computer"
+    }
+    return roundWinner;
+
+}
+// playing an entire game 
+let playGame = () => {
+    let p = prompt("Type 's' for scissors, 'r' for rock, 'p' for paper: ");
+    let humanChoice = (getHumanChoice(p));
+    // generating computer choice
+    let computerChoice = getComputerChoice();
+    //playing a round
+    console.log(humanChoice, computerChoice);
+    let roundWinner = playRound(computerChoice, humanChoice);
+    switch(roundWinner){
+        case "human":
+            humanScore++;
+            console.log(`You won the round! the score is Player: ${humanScore} , Computer: ${computerScore}`);
+            break;
+        case "computer":
+            computerScore++;
+            console.log(`You lost the round! the score is Player: ${humanScore} , Computer: ${computerScore}`);
+            break;
+        case "draw":
+            console.log(`You drawed! the score is Player: ${humanScore} , Computer: ${computerScore}`);
+            break;
+        default:
+            console.log("Undefined roundWinner input!");
+    }
+}
+// main program
+// initializing human and computer scores
+let humanScore = 0;
+let computerScore = 0;
+// initializing round counter
+let roundCounter = 0;
+// initialzing a loop to play until 5 rounds are played
+while(roundCounter<5){
+    playGame();
+    roundCounter++;
+}
+let gameWinner
+if (humanScore > computerScore){
+    gameWinner = "human"
+}
+else if(humanScore == computerScore)
+    {
+        gameWinner = "draw"
+    }
+else{
+    gameWinner = "computer"
+} 
+if (gameWinner == "human"){
+    console.log(`Congratz! you've won the game with score : Player: ${humanScore} , Computer: ${computerScore}`);
+}
+else if (gameWinner=="draw"){
+    console.log(`you've drawed the game with score : Player: ${humanScore} , Computer: ${computerScore}`);
+}
+else{
+    console.log(`Mannaggia! you've lost the game with score : Player: ${humanScore} , Computer: ${computerScore}`);
+}
