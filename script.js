9725cfc0408fbec2684bb7ac71c071748f3cd11c
grouchomarxc137-star <grouchomarxc137@gmail.com>
2025-10-27 22:40:53 +0100

    Added the logic for the computer choice based on Math.random library

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hello TOP");
+"use strict";
+// computer choice between 0 and 0.3 : rock | between 0.3 and 0.7 : paper | else: scissors
+let getComputerChoice = () => {
+    let n = Math.random();
+    let computerChoice;
+    if ((n <= 0.3))
+    {
+        computerChoice = "rock";
+    }
+    else if (n >= 0.3 && n <= 0.7)
+    {
+        computerChoice = "paper"
+    }
+    else{
+        computerChoice = "scissors"
+    }
+    return computerChoice;
+}
